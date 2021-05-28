978038091bfec79131613603213e4ff8c0464438
Salaimuniselvam <salaimuniselvam7@gmail.com>
2021-05-28 21:55:42 +0530

    Brick Builder Game.

--- src/BrickBreakerGame/GamePlay.java
+++ src/BrickBreakerGame/GamePlay.java
@@ -1,83 +1,167 @@
 package BrickBreakerGame;
 
 import java.awt.Color;
+
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Timer;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
 
 import javax.swing.JPanel;
 
-import sun.java2d.loops.FillRect;
 
-public class Gameplay extends JPanel implements KeyListener, ActionListener {
+
+public class GamePlay extends JPanel implements KeyListener, ActionListener {
+	
+	/**
+	 * 
+	 */
 	
 	private boolean play= false;
 	private int score=0;
-	private int totalBricks=28;// 21
+	private int totalBricks=21;
 	
 	private Timer timer;
-	private int delay=7; //8
+	private int delay=8; 
 	
 	private int playerX=310;
 	
-	private int ballposX=120;
-	private int ballposY=350;
-	private int ballXdir=-1;
-	private int ballYdir=-2;
+	private int ballposX= 120;
+	private int ballposY= 350;
+	private int ballXdir= -1;
+	private int ballYdir= -2;
 	
-	public  Gameplay() {
+	private MapGenerator map;
+	
+	public  GamePlay() {
+		map = new MapGenerator(3, 7);
 		addKeyListener(this);
 		setFocusable(true);
-		setFocusTraversalKeysEnables(false);
+//		setFocusTraversalKeysEnables(false);
 		timer = new Timer(delay, this);  
 		timer.start();
 	}
 	
-	public void print(Graphics g) {
+	public void paint(Graphics g) {
 		
 		g.setColor(Color.white);
 		g.fillRect(1,1,692,592);
 		
-		g.setColor(Color.blue);
-		g.fillRect(0,0,3,562);
-		g.fillRect(0,0,692,592);
+		map.draw((Graphics2D)g);
+		
+		g.setColor(Color.yellow);
+		g.fillRect(0,0,3,592);
+		g.fillRect(0,0,692,3);
 		g.fillRect(691,0,3,592);
 		
-		g.setColor(Color.black);
+		g.setColor(Color.blue);
 		g.fillRect(playerX,550,100,8);
 		
-		g.setColor(Color.DARK_GRAY);
+		g.setColor(Color.green);
 		g.fillOval(ballposX, ballposY, 20, 20);
+		
+		g.setColor(Color.black);
+		g.setFont(new Font("serif", Font.BOLD, 25));
+		g.drawString("" + score, 590, 30);
+		
+		if (totalBricks <= 0) {
+			play = false;
+			ballXdir =  0;
+			ballYdir =  0;
+			g.setColor(Color.green);
+			g.setFont(new Font("serif", Font.BOLD, 30));
+			g.drawString("You Won, Score: " + score, 190, 300);
+			
+			g.setFont(new Font("serif", Font.BOLD, 20));
+			g.drawString("Press Enter to Restart.", 230, 350);	
+			
+		}
+		
+		if(ballposY > 570) {
+			play = false;
+			ballXdir = 0;
+			ballYdir = 0;
+			g.setColor(Color.red);
+			g.setFont(new Font("serif", Font.BOLD, 30));
+			g.drawString("Game Over, Score: " + score, 190, 300);
+			
+			g.setFont(new Font("serif", Font.BOLD, 20));
+			g.drawString("Press Enter to Restart.", 230, 350);	
+		} 
+		
+		g.dispose();
 		
 		
 		
 	}
 	
 
-	private void setFocusTraversalKeysEnables(boolean b) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	private void setFocusable(boolean b) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	private void addKeyListener(Gameplay gameplay) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		
+		timer.start();
+		if(play) {
+			
+//			Ball Pedal Interaction
+			if(new Rectangle(ballposX,ballposY,20,30).intersects(new Rectangle(playerX,550,100,8) )) {
+				ballYdir = -ballYdir;
+			}
+			
+
+			for( int i = 0; i<map.map.length; i++) {
+				for(int j = 0; j<map.map[0].length; j++) {
+					if(map.map[i][j] > 0) {
+						int brickX = j*map.brickWidth + 80;
+						int brickY = i*map.brickHeight + 50;
+						int brickWidth= map.brickWidth;
+						int brickHeight = map.brickHeight;
+						
+						Rectangle rect = new Rectangle(brickX, brickY, brickWidth, brickHeight);
+						Rectangle ballRect = new Rectangle(ballposX, ballposY, 20,20);
+						Rectangle brickRect = rect;
+						
+						if(ballRect.intersects(brickRect) ) {
+							map.setBrickValue(0, i, j);
+							totalBricks--;
+							score+=5;
+							
+							if(ballposX + 19 <= brickRect.x || ballposX +1 >= brickRect.x + brickRect.width) 
+								ballXdir = -ballXdir;
+							 else {
+								ballYdir = -ballYdir;
+							}
+						}
+						
+					}
+					
+				}
+			}
+			
+			
+			ballposX += ballXdir;
+			ballposY += ballYdir;
+			
+			if(ballposX<0) {
+				ballXdir = -ballXdir;
+			}
+			
+			if(ballposY<0) {
+				ballYdir = -ballYdir;
+			}
+			if(ballposX>670) {
+				ballXdir = -ballXdir;
+			}
+		}
+		repaint();
 		
 	}
 
@@ -91,6 +175,48 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			if(playerX>=600) {
+				playerX=600;
+			} else {
+				moveRight();
+			}
+		}
+		
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			if(playerX<10) {
+				playerX=10;
+			} else {
+				moveLeft();
+			} 
+		}
+		
+	
+	if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+		if(!play) {
+			play = true;
+			ballposX = 120;
+			ballposY = 350;
+			ballXdir = -1;
+			ballYdir = -2;
+			score = 0;
+			totalBricks = 21;
+			map = new MapGenerator(3,7);
+			
+			repaint();
+		}
+	}
+	
+}	
+	
+	public void moveRight() {
+		play=true;
+		playerX +=20;
+	}
+	
+	public void moveLeft() {
+		play=true;
+		playerX -=20;
 	}
 
 	@Override
--- src/BrickBreakerGame/Main.java
+++ src/BrickBreakerGame/Main.java
@@ -2,17 +2,19 @@ package BrickBreakerGame;
 
 import javax.swing.JFrame;
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
 		
 		JFrame obj= new JFrame();
-		Gameplay gamePlay= new Gameplay();
-		obj.setBounds(10,10,700,600);
+		
+		GamePlay gamePlay = new GamePlay();
+		obj.setBounds(10, 10, 700, 600);
 		obj.setTitle("Brick Breaker");
 		obj.setResizable(false);
 		obj.setVisible(true);
 		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		obj.add(gamePlay);
 		
 		
 
